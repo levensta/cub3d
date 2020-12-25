ce80dc88192719e1054e6f2fc5d91d18fc812fe9
levensta <levensta@student.21-school.ru>
2020-12-25 22:33:21 +0300

    raycasting step0

--- Makefile
+++ Makefile
@@ -40,39 +40,72 @@
 
 # .PHONY: all bonus clean fclean re
 
+
+
+
+
+
+# NAME = cub3D
+# SRC_DIR = src/
+# OBJ_DIR = obj/
+# MLX_DIR = minilibx_mms/
+# INCLUDES = -I ./includes -I ./
+# CC = gcc
+# FLAGS = -Wall -Wextra -Werror
+# MLX_FLAGS = -g -L $(MLX_DIR) -lmlx -framework OpenGL -framework Appkit -lm
+# LIBFT = libft.a
+# SRCS =	cub3d.c \
+# 		ft_utils.c
+
+# SRC = $(addprefix $(SRC_DIR), $(SRCS))
+# OBJ = $(addprefix $(OBJ_DIR), $(SRCS:.c=.o))
+
+# all: $(NAME)
+
+# $(NAME):$(OBJ)
+# 	@make -C $(MLX_DIR)
+# 	@cp	$(MLX_DIR)libmlx.dylib ./
+# 	$(CC) libmlx.dylib $(INCLUDES) $(OBJ) $(MLX_FLAGS) -o $(NAME)
+
+# $(OBJ_DIR)%.o : $(SRC_DIR)%.c
+# 	@$(CC) $(FLAGS) $(INCLUDES) -I $(MLX_DIR) -c $< -o $@
+
+# clean:
+# 	@rm -rf $(OBJ)
+# 	@rm -f *.bmp
+
+# fclean: clean
+# 	@rm -f $(NAME)
+# 	@rm -f libmlx.dylib
+
+# re: fclean all
+
+# .PHONY: all clean fclean run re
+
+
+
+
 NAME = cub3D
-SRC_DIR = src/
-OBJ_DIR = obj/
-MLX_DIR = minilibx_mms/
-INCLUDES = -I ./includes -I ./
-CC = gcc
-FLAGS = -Wall -Wextra -Werror
-MLX_FLAGS = -L $(MLX_DIR) -lmlx -framework OpenGL -framework Appkit
-LIBFT = libft.a
-SRCS =	cub3d.c \
-		ft_utils.c
-
-SRC = $(addprefix $(SRC_DIR), $(SRCS))
-OBJ = $(addprefix $(OBJ_DIR), $(SRCS:.c=.o))
-
+SRCS = ./src/
+FUNC =	cub3d \
+		ft_utils
+		
+SRC = $(addprefix $(SRCS), $(addsuffix .c, $(FUNC)))
+OBJ = $(addprefix $(SRCS), $(addsuffix .o, $(FUNC)))
+FLAGS = -g -Wall -Werror -Wextra
 all: $(NAME)
-
-$(NAME):$(OBJ)
-	@make -C $(MLX_DIR)
-	@cp	$(MLX_DIR)libmlx.dylib ./
-	$(CC) $(INCLUDES) $(OBJ) $(MLX_FLAGS) -o $(NAME)
-
-$(OBJ_DIR)%.o : $(SRC_DIR)%.c
-	@$(CC) $(FLAGS) $(INCLUDES) -I $(MLX_DIR) -c $< -o $@
-
+$(NAME): $(OBJ)
+	$(MAKE) -C ./libft/
+	$(MAKE) -C ./minilibx_opengl
+	mv ./minilibx_opengl/libmlx.a .
+	mv ./libft/libft.a .
+	gcc -g -I. ./libft.a -lmlx -framework OpenGL -framework AppKit -lm $(OBJ) -o $(NAME)
+%.o: %.c
+	gcc -o $@ -c $<
 clean:
-	@rm -rf $(OBJ)
-	@rm -f *.bmp
-
+	$(MAKE) clean -C ./libft/
+	rm -f $(OBJ)
 fclean: clean
-	@rm -f $(NAME)
-	@rm -f libmlx.dylib
-
+	rm -f $(NAME) libft.a libmlx.a
 re: fclean all
-
-.PHONY: all clean fclean run re
+.PHONY: all clean fclean re
--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: levensta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 17:45:09 by levensta          #+#    #+#             */
-/*   Updated: 2020/12/21 20:33:45 by levensta         ###   ########.fr       */
+/*   Updated: 2020/12/25 22:27:18 by levensta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,56 +87,162 @@ int             key_hook(int keycode)
 
 int             close_win(int keycode, t_vars *vars)
 {
-	(void)keycode;
-    mlx_destroy_window(vars->mlx, vars->win);
+	if (keycode == 53)
+	{
+    	mlx_destroy_window(vars->mlx, vars->win);
+		exit(0);
+	}
 	return(0);
 }
 
+// int             mouse_print(int keycode, t_vars *vars)
+// {
+// 	(void)keycode;
+// 	vars->win = mlx_mouse_hide();
+// 	return(0);
+// }
+
+void	ray_correct(float *ray)
+{
+	while (*ray < 0)
+		*ray += 1;
+    while (*ray >= 1)
+		*ray -= 1;
+}
+
+#define mapWidth 24
+#define mapHeight 24
+#define screenWidth 640
+#define screenHeight 480
+
+int worldMap[mapWidth][mapHeight]=
+{
+  {1,1,1,1,1},
+  {1,0,0,0,1},
+  {1,0,0,0,1},
+  {1,0,0,0,1},
+  {1,1,1,1,1},
+};
+
 int main ()
 {
-    t_data	img;
+    t_data	win;
 	t_vars	vars;
+	int x = 0;
+	int wall = 0;
+    float fov = 60/360;
+	float x0 = 2.5, y0 = 2.5;  //x and y start position
+	float x1 = 0, y1 = 0;
+    float dx;
+    float dy;
+	float route = 0;
+	float distance = 0;
+    float ray = route - fov/2;
+	float column_h;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 500, 500, "labirint");
-	img.img = mlx_new_image(vars.mlx, 500, 500);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, \
-                                 &img.endian);
-	
-	// mlx_pixel_put(g_mlx, g_win, 250, 250, 0xFF0000);
-	int i = 30;
-	int j = 150;
-	int k = 151;
-	double r = 64;
-	double g = 201;
-	double blue = create_trgb(0, r, g, 255);
-	// int purple = create_trgb(0, 232, 28, 255);
-	while (i < 150)
-	{
-		my_mlx_pixel_put(&img, j, i, blue);
-		i++;
-		j = 150;
-		
-		while (j < k)
+	vars.win = mlx_new_window(vars.mlx, screenWidth, screenHeight, "labirint");
+	win.img = mlx_new_image(vars.mlx, screenWidth, screenHeight);
+	win.addr = mlx_get_data_addr(win.img, &win.bits_per_pixel, &win.line_length, \
+                                 &win.endian);
+
+    while (ray < route + fov/2)
+    {
+		x1 = x0;
+		y1 = y0;
+		ray_correct(&ray);
+		while (!wall)
 		{
-			my_mlx_pixel_put(&img, j, i, blue);
-			j++;
+			if (ray <= 0.5)
+				dx = ceilf(x1) - x1;
+			else
+				dx = x1 - floorf(x1);
+			if (ray >= 0.25 && ray < 0.75)
+				dy = ceilf(y1) - y1;
+			else   
+				dy = y1 - floorf(y1);
+
+
+			float a = tanf(ray * 2 * M_PI) * dy;
+			float b = tanf(M_PI_2 - ray * 2 * M_PI) * dx;
+			if (a < b)
+			{
+				if (ray <= 0.5)
+					x1 += a;
+				else
+					x1 -= a;
+				if (ray >= 0.25 && ray < 0.75)
+					y1 = ceilf(y1);
+				else
+					y1 = floorf(y1);
+			}
+			else
+			{
+				if (ray <= 0.5)
+					x1 = ceilf(x1);
+				else
+					x1 = floorf(x1);
+				if (ray >= 0.25 && ray < 0.75)
+					y1 += b;
+				else
+					y1 -= b;
+			}
+
+
+
+			if (ray >= 0.125 && ray < 0.375)
+			{
+				if (worldMap[(int)ceilf(x1)][(int)floor(y1)] == 1)
+					wall = 1;
+			}
+
+			if (ray >= 0.375 && ray < 0.625)
+			{
+				if (worldMap[(int)floorf(x1)][(int)ceilf(y1)] == 1)
+					wall = 1;
+			}
+
+			if (ray >= 0.625 && ray < 0.875)
+			{
+				if (worldMap[(int)floor(x1) - 1][(int)floor(y1)] == 1)
+					wall = 1;
+			}
+
+			else
+			{
+				if (worldMap[(int)floor(x1)][(int)floor(y1) - 1] == 1)
+					wall = 1;
+			}
+
 		}
-		if (r < 232.0)
-			r = r + (232.0 / 150.0);
-		if (g > 201.0)
-			g = g - (201.0 / 150.0);
-		blue = create_trgb(0, r, g, 255);
-		k++;
-	}
-	// circle(&img, 30, 30, 30, 0x00FF00FF);
-	// line(&img, 1, 500, 2, 499, 0x00FF00FF);
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	// mlx_key_hook(vars.win, &key_hook, NULL);
-	// mlx_hook(vars.win, 2, 1L<<0, &close_win, &vars);
+		distance = sqrtf(powf(x0 - x1, 2) + powf(y0 - y1, 2));
+
+
+		column_h = screenHeight / 2;
+        column_h = (float)column_h / tanf((fov / 2) * (M_PI / 180.0f));
+        column_h = (int)ceilf((float)column_h / distance);
+
+		int i = 0;
+		int j = screenHeight / 2;
+		while (i < screenWidth)
+		{
+			while (j < screenHeight)
+			{
+				my_mlx_pixel_put(&win, i, j, 0x00FF00FF);
+				j++;
+			}
+			i++;
+		}
+        ray += fov / screenWidth;
+    }
+
+	mlx_put_image_to_window(vars.mlx, vars.win, win.img, 0, 0);
+	mlx_key_hook(vars.win, key_hook, NULL);
+	mlx_hook(vars.win, 2, 1L<<0, &close_win, &vars);
+	// mlx_hook(vars.win, 9, 1L<<4, &mouse_print, &vars);
 	mlx_loop(vars.mlx);
 	return (0);
 }
 // (232 - r) / 150
 // Если требуется за 30 итераций пройти от числа 10 до 60. Шаг = (60-10)/30=5/3
-// gccw -I /usr/local/include cub3d.c -L /usr/local/lib -lmlx -framework OpenGL -framework AppKit
+// gccw -I /usr/local/include cub3d.c ft_utils.c -L /usr/local/lib -lmlx -framework OpenGL -framework AppKit
